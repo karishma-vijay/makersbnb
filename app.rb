c2d2608815e3d60d8c7d8e9e867b9242a1ae80ba
Karishma <karishmavijay21@gmail.com>
2022-09-07 09:53:19 +0100

    adding listings repo and listings, view_spaces

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative 'lib/listings_repository'
+require_relative 'lib/database_connection'
+DatabaseConnection.connect('makersbnb_test')
 
 class Application < Sinatra::Base
   # This allows the app code to refresh
@@ -8,8 +11,11 @@ class Application < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  get "/view-spaces" do
-    erb(:view_spaces,:layout => :layout)
+  get "/view_spaces" do
+    repo = ListingsRepository.new 
+    @listings = repo.all
+    erb(:view_spaces)
+    # binding.irb
   end
 end
 
---
+++ lib/listings.rb
@@ -0,0 +1,4 @@
+class Listings
+    attr_accessor :id, :name, :description, :price, :date_from, :date_to, :user_id
+end
+

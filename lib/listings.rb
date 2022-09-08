83aa02c8d031382a20127062c1d088b00948b306
Michael-Shiel <109136690+Michael-Shiel@users.noreply.github.com>
2022-09-08 11:48:52 +0100

    Revert "Just want to update my branch"

--- lib/listings.rb
+++
@@ -1,4 +0,0 @@
-class Listings
-    attr_accessor :id, :name, :description, :price, :date_from, :date_to, :user_id
-end
-
--- spec/seeds_bookings.sql
+++ spec/seeds_bookings.sql
@@ -1,5 +1,5 @@
 TRUNCATE TABLE bookings RESTART IDENTITY;
 
-INSERT INTO bookings (approval, user_id, check_in, check_out) VALUES (true, 1, '2022-09-15', '2022-09-22');
-INSERT INTO bookings (approval, user_id, check_in, check_out) VALUES (true, 2, '2022-07-15', '2022-07-22');
-INSERT INTO bookings (approval, user_id, check_in, check_out) VALUES (false, 3, '2022-06-15', '2022-06-22');
+INSERT INTO bookings (approval, listing_id, user_id, check_in, check_out) VALUES (true, 1, '2022-09-15', '2022-09-22');
+INSERT INTO bookings (approval, listing_id, user_id, check_in, check_out) VALUES (true, 2, '2022-07-15', '2022-07-22');
+INSERT INTO bookings (approval, listing_id, user_id, check_in, check_out) VALUES (false, 3, '2022-06-15', '2022-06-22');
